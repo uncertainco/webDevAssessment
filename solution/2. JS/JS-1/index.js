57ea07a91855af8d6c6b3e4612bd46fd35d23654
dryptm <vinay9415756155@gmail.com>
2021-06-23 21:10:48 +0530

    solutions

---
+++ solution/2. JS/JS-1/index.js
@@ -0,0 +1,8 @@
+var x=1000;
+var y=234;
+var sum=x+y;
+var message="the sum is ";
+message=message.concat(sum)
+alert(message)
+
+alert("the sum without declaring any variables is "+ (1000+234))
---
+++ solution/2. JS/JS-2/index.js
@@ -0,0 +1,13 @@
+function abc(first,last){
+    return (first+" "+last); 
+}
+function bcd(first,last){
+    first=prompt();
+    last=prompt();
+    var fullname=abc(first,last);
+    alert("your name is "+fullname)
+}
+bcd()
+
+
+// few statements in the task are written in a wrong way
